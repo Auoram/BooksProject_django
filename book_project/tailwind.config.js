0d1b5376e475d6be47b100d6122e30098a76a099
Auoram <marouaboumchich@gmail.com>
2024-09-23 21:13:07 +0200

    Navbar done

--- book_project/tailwind.config.js
+++ book_project/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./templates/**/*.html', './books/**/*.html'],
+  content: ['./**/templates/**/*.html', './books/**/*.html'],
   theme: {
     extend: {},
   },
