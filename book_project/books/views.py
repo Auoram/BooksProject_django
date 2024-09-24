1d0167b6e58ccd2b561b4bb0ee638fc5bf8f81ac
Auoram <marouaboumchich@gmail.com>
2024-09-24 20:51:45 +0200

    Books display done

--- book_project/books/views.py
+++ book_project/books/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Books,Author
-
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 def index(request):
